8960eb99e71b1343c3db3eafd34a240579325c7c
sirutio <sirutio03@gmail.com>
2024-08-13 23:52:07 +0900

    Title: 특정 문자의 등장 횟수, Time: 5023ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240813/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
+++ 240813/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -1,11 +1,15 @@
 string = input()
 cnt = 0
-idx = 0
+idx = string.index('ee')
 
 while string[idx:].find('ee') != -1:
-    idx = string.index('ee') + 1 
+    idx += 1 
     cnt += 1 
 print(cnt,end=' ')
+
+cnt = 0
+idx = string.index('eb')
+
 while string[idx:].find('eb') != -1:
     idx = string.index('eb') + 1 
     cnt += 1 
