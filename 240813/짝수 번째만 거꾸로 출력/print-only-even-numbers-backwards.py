a6026bc9ba74737a7a786a3a1cab466b3e2cf0dd
sirutio <sirutio03@gmail.com>
2024-08-13 19:17:05 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240813/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 240813/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -1,5 +1,9 @@
 string = input()
 
 for i,char in enumerate(string[::-1]):
-    if i%2 == 0:
-        print(char,end='')
+    if len(string)%2 == 0:
+        if i%2 == 0:
+            print(char,end='')
+    else:
+        if i%2 != 0:
+            print(char,end='')
