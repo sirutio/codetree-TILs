1e66b9b710cef1b64294b42be2139342eba75843
sirutio <sirutio03@gmail.com>
2024-08-16 15:31:38 +0900

    Title: 최대공약수 구하기, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240816/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,11 @@
 def CommonDivisor(n,m):
     if n >= m:
-        for i in range(m,1,-1):
+        for i in range(m,0,-1):
             if n%i == 0 and m%i == 0:
                 print(i)
                 break
     else:
-        for i in range(n,1,-1):
+        for i in range(n,0,-1):
             if m%i == 0 and n%i == 0:
                 print(i)
                 break
