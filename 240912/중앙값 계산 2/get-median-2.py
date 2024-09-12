f3eba00300eb5905e1be45901aa8cd794d6645b6
sirutio <sirutio03@gmail.com>
2024-09-12 21:15:15 +0900

    Title: 중앙값 계산 2, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 240912/중앙값 계산 2/get-median-2.py
+++ 240912/중앙값 계산 2/get-median-2.py
@@ -1,9 +1,10 @@
 n = int(input())
 arr = list(map(int,input().split()))
-arr.sort()
 
 for i in range(n):
     if i == 0:
         print(arr[0], end=' ')
     elif i%2 == 0:
-        print(arr[i//2], end=' ')
+        new_arr = arr[:i+1]
+        new_arr.sort()
+        print(new_arr[i//2], end=' ')
