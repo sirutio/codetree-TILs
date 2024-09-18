cb071d129a767d57f1ed1f9467b2469dba65cd73
sirutio <sirutio03@gmail.com>
2024-09-18 23:34:17 +0900

    Title: 비오는 날, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240918/비오는 날/rainy-day.py
+++ 240918/비오는 날/rainy-day.py
@@ -8,7 +8,22 @@ n = int(input())
 arr = [tuple(input().split()) for _ in range(n)]
 data = [info(date,day,weather) for date,day,weather in arr]
 
+def IsRecent(y,m,d,recent_date):
+    ry,rm,rd = recent_date[:4],recent_date[5:7], recent_date[8:]
+    if int(y) < int(ry):
+        if int(m) < int(rm):
+            if int(d) < int(rd):
+                return True
+    return False
+
+recent_idx = 0
+recent_date = '2101-13-32'
 for i,information in enumerate(data):
     if information.weather == 'Rain':
-        print(information.date, information.day, 'Rain')
-        break
+        date_str = information.date
+        y,m,d = date_str[:4],date_str[5:7], date_str[8:]
+        if IsRecent(y,m,d,recent_date):
+            recent_idx = i
+            recent_date = date_str 
+    
+print(data[recent_idx].date, data[recent_idx].day, data[recent_idx].weather)
