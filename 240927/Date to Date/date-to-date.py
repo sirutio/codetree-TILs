6fd7b49725a3d895f4e9c5eee0cdc49a2746124a
sirutio <sirutio03@gmail.com>
2024-09-27 14:59:46 +0900

    Title: Date to Date, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240927/Date to Date/date-to-date.py
+++ 240927/Date to Date/date-to-date.py
@@ -1,15 +1,15 @@
 m1,d1,m2,d2 = map(int, input().split())
-arr = [31,30,31,30,31,30,31,31,30,31,30,31]
+arr = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 temp1 = 0
 for i in range(m1-1):
     temp1 += arr[i]
-temp1 += d1-1
+temp1 += d1
 
 
 temp2 = 0
 for i in range(m2-1):
     temp2 += arr[i]
-temp2 += d2-1
+temp2 += d2
 
 print(temp2-temp1+1)
