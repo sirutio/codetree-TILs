98fc906b5a9f572569e6f00ac4aa4b788016213e
sirutio <sirutio03@gmail.com>
2024-09-28 22:22:00 +0900

    Title: 십진수와 이진수 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240928/십진수와 이진수 2/decimal-and-binary-number-2.py
+++ 240928/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -1,10 +1,8 @@
 N = input()
 num = 0
-i = 0
 
-for digit in N[::-1]:
-    num += int(digit)*(2**i)
-    i += 1
+for digit in N:
+    num = num*2 + int(digit)
 num *= 17
 
 dem = []
@@ -12,7 +10,7 @@ while 1:
     if num < 2:
         dem.append(num)
         break
-    dem.append(num%2)
+    dem.append(num%2) 
     num //= 2
 for digit in dem[::-1]:
     print(digit,end='')
