fd7fac1f209640ec29c66d8fb2479a49b25fc970
sirutio <sirutio03@gmail.com>
2024-09-29 21:56:19 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240929/요일 맞추기/guess-day-of-week.py
+++ 240929/요일 맞추기/guess-day-of-week.py
@@ -1,8 +1,17 @@
 m1, d1, m2, d2 = map(int,input().split())
+if m1 > m2:
+    m1,m2 = m2,m1
+    d1,d2 = d2,d1
+    Reversed = True
 days = [31,28,31,30,31,30,31,31,30,31,30,31]
+day7 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 gap = 0
+
 for i in range(m1-1,m2-1):
     gap += days[i]
-gap += d2-d1 
-day7 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-print(day7[gap%7])
+gap += d2-d1
+
+if not Reversed:
+    print(day7[gap%7])
+else:
+    print(day7[-gap%7])
