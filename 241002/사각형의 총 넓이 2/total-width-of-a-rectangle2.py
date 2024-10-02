6644402be3670dee706929403aee979a4e9f19be
sirutio <sirutio03@gmail.com>
2024-10-02 13:23:45 +0900

    Title: 사각형의 총 넓이 2, Time: 158ms, Memory: 28MB, Status: Passed - CodeTree

--- 241002/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 241002/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -5,7 +5,7 @@ for _ in range(N):
     x1,y1,x2,y2 = map(int, input().split())
     for y in range(y1,y2):
         for x in range(x1,x2):
-            arr[y][x] += 1
+            arr[y+100][x+100] += 1
 width = 0
 for i in range(201):
     for j in range(201):
