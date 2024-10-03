2830dd20b483ddf678cb1b5f2f52d6cef7e8f54b
sirutio <sirutio03@gmail.com>
2024-10-03 23:45:11 +0900

    Title: 연속되는 수 2, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241003/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = []
+for _ in range(N):
+    num = int(input())
+    arr.append(num)
+
+count = 0
+ans = 0
+for i in range(N):
+    if i == 0 or  arr[i] == arr[i-1]:
+        count += 1
+    else:
+        if count > ans:
+            ans = count
+        count = 1
+        
+print(ans)
