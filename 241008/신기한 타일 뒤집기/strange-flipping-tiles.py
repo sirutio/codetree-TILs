b1dd48ffe33454d0810bdcbfc8bc77034a5c2e1c
sirutio <sirutio03@gmail.com>
2024-10-08 14:28:17 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241008/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 241008/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -4,7 +4,7 @@ L 흰 R 검 현재 위치 포함
 OFFSET = 10
 n = int(input())
 cur = OFFSET
-arr = [-1 for _ in range(OFFSET*2)]
+arr = [-1 for _ in range(OFFSET*2+2)]
 
 for _ in range(n):
     x,way = input().split()
