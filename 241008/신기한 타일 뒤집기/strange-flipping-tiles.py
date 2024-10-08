d4f8a8533851f2697b7e1ee71046aff7cd06e38e
sirutio <sirutio03@gmail.com>
2024-10-08 14:21:57 +0900

    Title: 신기한 타일 뒤집기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,26 @@
+'''
+L 흰 R 검 현재 위치 포함
+'''
+OFFSET = 10
+n = int(input())
+cur = OFFSET
+arr = [-1 for _ in range(OFFSET*2)]
+
+for _ in range(n):
+    x,way = input().split()
+    if way == 'L':
+        for i in range(int(x)):
+            arr[cur-i] = 0
+        print(arr)
+    else:
+        for i in range(int(x)):
+            arr[cur+i] = 1
+        print(arr)
+
+w,b = 0,0
+for tile in arr:
+    if tile == 1:
+        b += 1
+    elif tile == 0:
+        w += 0
+print(w,b)
