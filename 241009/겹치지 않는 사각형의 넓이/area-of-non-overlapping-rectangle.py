0ed44e902c55ebfa54ec2db18417b124e038d41d
sirutio <sirutio03@gmail.com>
2024-10-09 20:12:39 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 251ms, Memory: 55MB, Status: Passed - CodeTree

---
+++ 241009/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -0,0 +1,21 @@
+OFFSET = 1000
+arr = [[0 for _ in range(OFFSET*2+1)] for _ in range(OFFSET*2+1)]
+
+for _ in range(2):
+    x1,y1,x2,y2 = map(int,input().split())
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            arr[i][j] = 1
+x1,y1,x2,y2 = map(int,input().split())
+
+for i in range(y1,y2):
+        for j in range(x1,x2):
+            arr[i][j] = 0
+
+ans = 0
+for row in arr:
+    for col in row:
+        if col == 1:
+            ans += 1
+    
+print(ans)
