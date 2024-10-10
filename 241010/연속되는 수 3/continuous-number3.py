430ed6a3036b9c064cb8b3d084965a1b3ed319a4
sirutio <sirutio03@gmail.com>
2024-10-10 16:36:04 +0900

    Title: 연속되는 수 3, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/연속되는 수 3/continuous-number3.py
+++ 241010/연속되는 수 3/continuous-number3.py
@@ -5,7 +5,7 @@ arr = []
 for i in range(N):
     num = int(input())
     arr.append(num)
-    if i != 0 and (num < 0 and arr[i-1] < 0) or (num > 0 and arr[i-1] > 0):
+    if i != 0 and arr[i]*arr[i-1] > 0:
         cnt += 1
     else:
         cnt = 1
