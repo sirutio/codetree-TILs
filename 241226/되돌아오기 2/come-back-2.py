e1c30b8842aa6507e2a4e8b5e41cb05c286e2d9d
sirutio <sirutio03@gmail.com>
2024-12-26 14:34:46 +0900

    Title: 되돌아오기 2, Time: 55ms, Memory: 17MB, Status: Passed - CodeTree

--- 241226/되돌아오기 2/come-back-2.py
+++ 241226/되돌아오기 2/come-back-2.py
@@ -4,6 +4,7 @@ x,y = 0,0
 current_dir = 3 # 북쪽 
 ans = 0
 
+IsBack = False
 for order in string:
     if order == 'L':
         current_dir = current_dir%4 - 1
@@ -13,7 +14,8 @@ for order in string:
         x,y = x+dx[current_dir], y+dy[current_dir]
         if x == 0 and y == 0:
             print(ans+1)
+            IsBack = True
             break
     ans += 1
-    
-print(-1)
+if not IsBack:
+    print(-1)
